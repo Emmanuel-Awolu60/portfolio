293c5db03200e3ba1b165f795162f63c629e6040
Emmanuel-Awolu60 <emmanuelawolu8@gmail.com>
2024-12-21 14:00:20 +0100

    Added the Service files

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import './App.css'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
+import Services from './components/Ser/Services'
+// import Ser from './components/Ser/Services'
+
 function App() {
   return (
     <>
       <Header />
       <Hero />
       <About />
+      <Services />
     </>
   )
 }
