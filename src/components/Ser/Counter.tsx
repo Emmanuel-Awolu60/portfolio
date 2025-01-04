549b1961cc6d7910de411c334fd14573d229069e
Emmanuel-Awolu60 <emmanuelawolu8@gmail.com>
2025-01-04 15:08:29 +0100

    updated

--- src/components/Ser/Counter.tsx
+++ src/components/Ser/Counter.tsx
@@ -1,7 +1,41 @@
 import "./Counter.css"
+import { FaLaptopCode } from "react-icons/fa";
+import { FcProcess } from "react-icons/fc";
+import { LuPackageOpen } from "react-icons/lu";
+import { RiEmotionHappyLine } from "react-icons/ri";
 const Counter = () => {
     return (
-        <div>Counter</div>
+        <section>
+            <div className="flex ">
+                <nav>
+                    <nav>
+                        <FaLaptopCode />
+                    </nav>
+                    <p>
+                        Completed projects
+                    </p>
+                </nav>
+                <nav>
+                    <nav>
+                        <FcProcess />
+                    </nav>
+                    <p>
+                        Processes
+                    </p>
+                </nav>
+
+                <nav>
+                    <nav>
+                        <LuPackageOpen />
+                    </nav>
+                    <p>Awards Received</p>
+                </nav>
+                <nav>
+                    <nav><RiEmotionHappyLine /></nav>
+                    <p>Happy Clients</p>
+                </nav>
+            </div>
+        </section>
     )
 }
 
