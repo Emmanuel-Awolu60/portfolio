5aec86d6d6e4569258dd05f24b2481d2e1e20332
Emmanuel-Awolu60 <emmanuelawolu8@gmail.com>
2025-01-09 11:00:12 +0100

    updated

--- src/components/Ser/Counter.tsx
+++ src/components/Ser/Counter.tsx
@@ -3,7 +3,7 @@ import { FaLaptopCode } from "react-icons/fa";
 import { FcProcess } from "react-icons/fc";
 import { LuPackageOpen } from "react-icons/lu";
 import { RiEmotionHappyLine } from "react-icons/ri";
-
+import CountUp from "react-countup"
 
 const Counter = () => {
     return (
@@ -13,6 +13,7 @@ const Counter = () => {
                     <div className="text-4xl mb-2">
                         <FaLaptopCode />
                     </div>
+                    <CountUp start={8800} end={9000} duration={4} />
                     <h4>Completed projects</h4>
                 </nav>
 
@@ -20,6 +21,8 @@ const Counter = () => {
                     <div className="text-4xl mb-2">
                         <FcProcess />
                     </div>
+                    <CountUp start={8800} end={9000} duration={6} />
+
                     <h4>Processes</h4>
                 </nav>
 
@@ -27,6 +30,8 @@ const Counter = () => {
                     <div className="text-4xl mb-2">
                         <LuPackageOpen />
                     </div>
+                    <CountUp start={8800} end={9000} duration={4} />
+
                     <h4>Awards Received</h4>
                 </nav>
 
@@ -34,6 +39,8 @@ const Counter = () => {
                     <div className="text-4xl mb-2">
                         <RiEmotionHappyLine />
                     </div>
+                    <CountUp start={8800} end={9000} duration={4} />
+
                     <h4>Happy Clients</h4>
                 </nav>
             </div>
