d1381db3e2af36cbe9fb52dbe91fec497cb64704
dev_eao <114992756+Emmanuel-Awolu60@users.noreply.github.com>
2025-01-14 12:12:34 +0000

    make some changes to the testimonial section

--- src/components/Ser/Testimonials/Testimonials.tsx
+++ src/components/Ser/Testimonials/Testimonials.tsx
@@ -1,16 +1,11 @@
-// import React from 'react'
 import "./Testimonials.css"
-// import React, { useRef, useState } from 'react';
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
+// import { Navigation } from 'swiper';
+import { Pagination } from 'swiper';
 
 
-// import required modules
-import { Navigation } from 'swiper/modules';
 
 
 const Testimonials = () => {
@@ -23,11 +18,22 @@ const Testimonials = () => {
                 </div>
 
                 <div className="mt-20 w-3/4 m-auto">
-                    <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+                    <Swiper
+                        pagination={{
+                            dynamicBullets: true,
+                        }}
+                        modules={[Pagination]}
+                        className="mySwiper"
+                    >
                         <SwiperSlide>Slide 1</SwiperSlide>
                         <SwiperSlide>Slide 2</SwiperSlide>
                         <SwiperSlide>Slide 3</SwiperSlide>
                         <SwiperSlide>Slide 4</SwiperSlide>
+                        <SwiperSlide>Slide 5</SwiperSlide>
+                        <SwiperSlide>Slide 6</SwiperSlide>
+                        <SwiperSlide>Slide 7</SwiperSlide>
+                        <SwiperSlide>Slide 8</SwiperSlide>
+                        <SwiperSlide>Slide 9</SwiperSlide>
                     </Swiper>
                 </div>
 
