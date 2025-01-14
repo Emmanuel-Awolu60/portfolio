e37c3c5d9fa09fbf0fc342a3ceac2be6948714ef
dev_eao <114992756+Emmanuel-Awolu60@users.noreply.github.com>
2025-01-14 11:36:46 +0000

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
-// import Services from './components/Ser/Services'
+import Services from './components/Ser/Services'
 import Counter from './components/Ser/Counter'
 // import Testimonials from './components/Hero/Testimonials/Testimonials'
 // import Ser from './components/Ser/Services'
@@ -13,7 +13,7 @@ function App() {
       <Header />
       <Hero />
       <About />
-      {/* <Services /> */}
+      <Services />
       <Counter />
       {/* <Testimonials /> */}
     </>
--- src/components/Ser/Services.tsx
+++ src/components/Ser/Services.tsx
@@ -5,25 +5,64 @@ import { FaLaptopCode } from "react-icons/fa";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import { Pagination } from "swiper/modules";
+import { Pagination } from "swiper";
+
 
 // Import the reusable SlideContent component
 import SlideContent from "./SlideContent";
 
 const Services: React.FC = () => {
-    const handleButtonClick = (step: string) => {
-        alert(`Button clicked for step ${step}!`);
-    };
+    // Handle button click action
+    const handleButtonClick = (step: string) => alert(`Button clicked for step ${step}!`);
+
+    // Service data
+    const services = [
+        {
+            icon: <TbPencilCheck />,
+            step: "01",
+            title: "Tech Writer",
+            description: "Crafting engaging, SEO-optimized content to simplify complex tech concepts.",
+            buttonText: "Read More",
+        },
+        {
+            icon: <FaLaptopCode />,
+            step: "02",
+            title: "Frontend Development",
+            description: "Building interactive and responsive user interfaces using modern web technologies.",
+            buttonText: "Explore",
+        },
+        {
+            icon: <TbPencilCheck />,
+            step: "03",
+            title: "Backend Development",
+            description: "Building robust, scalable server-side solutions to power seamless  experiences.",
+            buttonText: "Contact Me",
+        },
+        {
+            icon: <TbDeviceAnalytics />,
+            step: "04",
+            title: "Research & Analysis",
+            description: "Driving insights through data research and analysis for informed decisions.",
+            buttonText: "Read More",
+        },
+        {
+            icon: <TbPencilCheck />,
+            step: "05",
+            title: "Consulting",
+            description: "Providing tailored solutions to optimize your business processes and technology stack.",
+            buttonText: "Contact Me",
+        },
+    ];
 
     return (
         <section className="mt-10">
-            <div className="mt-16 w-11/12 m-0 mx-auto">
-                <div className="flex justify-center items-center flex-col">
+            <div className="mt-16 w-11/12 m-auto">
+                <header className="flex justify-center items-center flex-col">
                     <h3 className="title-small">Service</h3>
                     <h1 className="text-4xl leading-9">What I do for you</h1>
-                </div>
+                </header>
 
-                <div className="top-top mt-12">
+                <div className="mt-12">
                     <Swiper
                         spaceBetween={30}
                         pagination={{ clickable: true }}
@@ -35,51 +74,15 @@ const Services: React.FC = () => {
                             0: { slidesPerView: 1 },    // Small screens
                         }}
                     >
-                        {[
-                            {
-                                icon: <TbPencilCheck />,
-                                step: "01",
-                                title: "Tech Writer",
-                                description: "Crafting engaging, SEO-optimized content to simplify complex tech concepts.",
-                                buttonText: "Read More",
-                            },
-                            {
-                                icon: <FaLaptopCode />,
-                                step: "02",
-                                title: "Frontend Development",
-                                description: "Building interactive and responsive user interfaces using modern web technologies.",
-                                buttonText: "Explore",
-                            },
-                            {
-                                icon: <TbPencilCheck />,
-                                step: "03",
-                                title: "Backend Development",
-                                description: "Building robust, scalable server-side solutions to power seamless user experiences.",
-                                buttonText: "Contact Me",
-                            },
-                            {
-                                icon: <TbDeviceAnalytics />,
-                                step: "04",
-                                title: "Research & Analysis",
-                                description: "Driving insights through data research and analysis for informed decisions.",
-                                buttonText: "Read More",
-                            },
-                            {
-                                icon: <TbPencilCheck />,
-                                step: "05",
-                                title: "Consulting",
-                                description: "Providing tailored solutions to optimize your business processes and technology stack.",
-                                buttonText: "Contact Me",
-                            },
-                        ].map((item, index) => (
+                        {services.map((service, index) => (
                             <SwiperSlide key={index}>
                                 <SlideContent
-                                    icon={item.icon}
-                                    step={item.step}
-                                    title={item.title}
-                                    description={item.description}
-                                    buttonText={item.buttonText}
-                                    onButtonClick={() => handleButtonClick(item.step)}
+                                    icon={service.icon}
+                                    step={service.step}
+                                    title={service.title}
+                                    description={service.description}
+                                    buttonText={service.buttonText}
+                                    onButtonClick={() => handleButtonClick(service.step)}
                                 />
                             </SwiperSlide>
                         ))}
