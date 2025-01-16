00b7394a4b108332d8b11993996db59da84c0af2
dev_eao <114992756+Emmanuel-Awolu60@users.noreply.github.com>
2025-01-16 11:38:51 +0000

    updated

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -3,14 +3,13 @@ import "./Contact.css"
 const Contact = () => {
     return (
         <>
-            <div>
-                <div>
-                    <div>
-                        <h4>Get in touch with Me</h4>
-                        <h1>Are you ready to work together?</h1>
-                        <p>Let's start a project! Hire Me.</p>
-                    </div>
-
+            <div className="">
+                <header className="mb-12 mt-32 flex justify-center items-center flex-col">
+                    <h4 className="text-xl title-small leading-9">Get in touch with Me</h4>
+                    <h1 className="text-4xl leading-9 pb-2">Are you ready to work together?</h1>
+                    <p className="title-small">Let's start a project! Hire Me.</p>
+                </header>
+                <div className=" w-11/12 mx-auto flex">
                     <div>
                         <p>I’m open to any communication! Feel free to contact me any convenient way! I’m always interested in hearing about new projects and opportunities.</p>
                         <span>
@@ -24,43 +23,39 @@ const Contact = () => {
 
 
                     <div className="container col-lg-7 mt-lg-0 mt-md-5 mt-4 HomePageContactForm">
-                        <div className="max-w-">
+                        <form action="" method="post">
+                            <div className="twice-two">
 
-                            <form action="" method="post">
-                                <div className="twice-two">
-
-                                    <input type="text" className="form-control"
-                                        name="w3lName" id="w3lName"
-                                        placeholder="Name" required>
+                                <input type="text" className="form-control"
+                                    name="w3lName" id="w3lName"
+                                    placeholder="Name" required>
+                                </input>
+                                <div>
+                                    <input type="email" className="form-control"
+                                        name="w3lSender" id="w3lSender"
+                                        placeholder="Email" required>
                                     </input>
-                                    <div>
-                                        <input type="email" className="form-control"
-                                            name="w3lSender" id="w3lSender"
-                                            placeholder="Email" required>
-                                        </input>
-                                    </div>
-                                    <div className="twice">
-                                        <input type="text" className="form-control"
-                                            name="w3lSubject" id="w3lSubject"
-                                            placeholder="Subject"
-                                            required>
-                                        </input>
-                                    </div>
-                                    <textarea name="w3lMessage"
-                                        className="form-control" id="w3lMessage"
-                                        placeholder="Message"
-                                        required>
-                                    </textarea>
-
-                                    <div className="text-right">
-                                        <button type="submit" className="btn btn-secondary btn-style mt-4">
-                                            Send Message</button>
-                                    </div>
-
                                 </div>
-                            </form>
-                        </div>
+                                <div className="twice">
+                                    <input type="text" className="form-control"
+                                        name="w3lSubject" id="w3lSubject"
+                                        placeholder="Subject"
+                                        required>
+                                    </input>
+                                </div>
+                                <textarea name="w3lMessage"
+                                    className="form-control" id="w3lMessage"
+                                    placeholder="Message"
+                                    required>
+                                </textarea>
 
+                                <div className="text-right">
+                                    <button type="submit" className="btn btn-secondary btn-style mt-4">
+                                        Send Message</button>
+                                </div>
+
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
