6a2adc598021ed3865286e7b7264b2512ff1fa31
dev_eao <114992756+Emmanuel-Awolu60@users.noreply.github.com>
2025-01-27 13:15:24 +0000

    updated the footer section

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -6,26 +6,30 @@ import { LuGithub } from "react-icons/lu";
 const Footer = () => {
     return (
         <footer className='w-full h-20'>
-            <div className='mt-16 w-11/12 m-auto footer-div flex'>
+            <div className='mt-16 w-4/5 m-auto footer-div flex justify-between items-center '>
 
                 <div className='text-white'>
                     <p className='text-white'>
-                        © Copyright 2025 Portfolio. Design by Emmanuel Awolu Oluwapelumi
+                        © Copyright 2025 Portfolio. Design by
+                        <a href="https://linktr.ee/Emmanuel_Awolu" target='_blank' className='ml-2'>Emmanuel Awolu Oluwapelumi.</a>
                     </p>
                 </div>
 
                 <div>
                     <li className='list-none'>
                         <p className='flex text-white'>
-                            <a href="">
-                                <FaLinkedinIn />
-                            </a>
-                            <a className="ml-2">
-                                <FaXTwitter />
-                            </a>
-                            <a href="https://github.com/Emmanuel-Awolu60">
+                            <a href="https://github.com/Emmanuel-Awolu60" target="_blank">
                                 <LuGithub />
                             </a>
+                            <a href='https://x.com/dev__eao' className="ml-2 mr-2" target="_blank">
+                                <FaXTwitter />
+                            </a>
+
+                            <a href="https://www.linkedin.com/in/emmanuel-awolu-463304254/" target="_blank">
+                                <FaLinkedinIn />
+                            </a>
+
+
                         </p>
                     </li>
                 </div>
