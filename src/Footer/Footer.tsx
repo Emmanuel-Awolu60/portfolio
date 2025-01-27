fc4a4e24dac634e4a9ac22485fd6b31e46cf3de2
dev_eao <114992756+Emmanuel-Awolu60@users.noreply.github.com>
2025-01-27 12:28:37 +0000

    updated

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import "./Footer.css"
 const Footer = () => {
     return (
-        <div>
-            <footer className=''>
+        <footer className='w-full h-20 bg-black'>
+            <div className='mt-16 w-11/12 m-auto bg-slate-600 footer-div'>
 
-            </footer>
-        </div>
+            </div>
+        </footer>
     )
 }
 
