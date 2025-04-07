b575ca97bd7877deb6bfa74c7cc2f74ccf74a226
dev_eao <114992756+Emmanuel-Awolu60@users.noreply.github.com>
2025-04-07 19:43:41 +0100

    Updated

--- src/components/Ser/SlideContent.tsx
+++ src/components/Ser/SlideContent.tsx
@@ -1,44 +1,38 @@
 // SlideContent.tsx
-// import React from 'react';
-import "./SlideContent.css"
+import './SlideContent.css';
 
 interface SlideContentProps {
-    icon: React.ReactNode;
-    step: string;
-    title: string;
-    description: string;
-    buttonText?: string;
-    onButtonClick?: () => void;
+  icon: React.ReactNode;
+  step: string;
+  title: string;
+  description: string;
+  buttonText?: string;
+  onButtonClick?: () => void;
 }
 
 const SlideContent: React.FC<SlideContentProps> = ({
-    icon,
-    step,
-    title,
-    description,
-    buttonText = 'Learn More',
-    onButtonClick = () => { },
+  icon,
+  step,
+  title,
+  description,
+  buttonText = 'Learn More',
+  onButtonClick = () => {},
 }) => {
-    return (
-        <>
-            <main className='main-container top-div py-10 px-9 mb-16 rounded-lg transition-all relative md:block sm:block'>
-                <div className="flex md:block sm:block items-center mb-10">
-                    <span className="T-icon text-3xl mr-2">{icon}</span>
-                    <h1 className="number absolute text-6xl font-bold">{step}</h1>
-                </div>
-                <h2 className="text-2xl font-semibold mb-2">{title}</h2>
-                <p className="text-gray-600 mb-4">{description}</p>
-                <button
-                    className="ServicesBtn"
-                    onClick={onButtonClick}
-                >
-                    {buttonText}
-                </button>
-            </main>
-        </>
-    );
+  return (
+    <>
+      <main className="main-container top-div py-10 px-9 mb-16 rounded-lg transition-all relative md:block sm:block">
+        <div className="flex md:block sm:block items-center mb-10">
+          <span className="T-icon text-3xl mr-2">{icon}</span>
+          <h1 className="number absolute text-6xl font-bold">{step}</h1>
+        </div>
+        <h2 className="text-2xl font-semibold mb-2">{title}</h2>
+        <p className="text-gray-600 mb-4">{description}</p>
+        <button className="ServicesBtn" onClick={onButtonClick}>
+          {buttonText}
+        </button>
+      </main>
+    </>
+  );
 };
 
 export default SlideContent;
-
-
